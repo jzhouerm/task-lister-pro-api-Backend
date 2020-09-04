c9fed00cd0235f539d48324961d494fde834a82d
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-09-04 12:19:58 -0400

    09-04-20 -mike- controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show, :update]
+    def index
+        users = User.all
+        render json: users, except: [:created_at, :updated_at]
+    end
+    
+    def show
+        render json: user, except: [:created_at, :updated_at]
+    end
+    
+    def create
+        user = User.create!(user_params)
+        render json: user, except: [:created_at, :updated_at] 
+    end
+    
+    def update
+        user.update!(user_params)
+        render json: user, except: [:created_at, :updated_at]
+    end
+    
+    private
+        
+    def find_user
+        user = User.find(params[:id])
+    end
+    
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
+    
 end
